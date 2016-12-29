177c8a832e7baa592d9b316afaf3e10eea3927c1
Rasmus Appelqvist <rasmus.a-91@hotmail.com>
2016-12-29 21:15:42 +0100

    Added tab layout, classes and added them to the section adapter

---
+++ app/src/main/java/com/superteam/klockan/TabStopwatch.java
@@ -0,0 +1,22 @@
+package com.superteam.klockan;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by Rasmus on 2016-12-29.
+ */
+
+public class TabStopwatch extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.stopwatch_layout, container, false);
+
+        return rootView;
+    }
+}
