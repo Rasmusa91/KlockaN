4a74d7e1e228acdc8a2777790c9dd4ba34f1e436
William Rosander <william.rosander95@gmail.com>
2017-01-07 03:40:53 +0100

    Added functionality to lap several times, also cleanup and smaller changes to existing code

--- app/src/main/java/com/superteam/klockan/TabStopwatch.java
+++ app/src/main/java/com/superteam/klockan/TabStopwatch.java
@@ -1,23 +1,15 @@
 package com.superteam.klockan;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.SystemClock;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
 
 /**
@@ -26,14 +18,21 @@ import java.util.ArrayList;
 
 public class TabStopwatch extends TabFragment
 {
-    private ArrayList<AlarmObject> m_Items;
-    private ArrayAdapter<AlarmObject> m_Adapter;
     private View m_View;
+    private String intNumbers[] = {"00","01","02","03","04","05","06","07","08","09","10","11","12","13","14",
+                        "15","16","17","18","19"};
+    private String txtSingleNumber[] = {"zero","one","two","three","four","five","six","seven","eight","nine",
+                            "ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen",
+                            "eighteen","nineteen"};
+    private String txtTenNumbers[] = {"","","twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
 
-    Button btnStart, btnPause, btnLap;
-    TextView txtTimer;
-    Handler customHandler = new Handler();
-    LinearLayout container;
+    private Button btnStart, btnPause, btnLap;
+    private TextView txtTimer;
+    private Handler customHandler = new Handler();
+
+    private ArrayList<String> arrayList;
+    private ListView listView;
+    private ArrayAdapter adapter;
 
     long    startTime= 0L,
             timeInMilliseconds=0L,
@@ -48,11 +47,8 @@ public class TabStopwatch extends TabFragment
             int milliseconds = (int) (updateTime%100);
             int secs=(int)(updateTime/1000);
             int mins=secs/60;
-            int hours=mins/24;
             secs%=60;
-
-            txtTimer.setText(String.format("%02d",hours)+":"
-                            +String.format("%02d",mins)+":"
+            txtTimer.setText(String.format("%02d",mins)+":"
                             +String.format("%02d",secs)+":"
                             +String.format("%02d",milliseconds));
             customHandler.postDelayed(this,0);
@@ -73,21 +69,12 @@ public class TabStopwatch extends TabFragment
     }
 
     private void initialize() {
-        m_Items = Preferences.getAllAlarms(getContext());
-
-        m_Adapter = new ArrayAdapter<AlarmObject>(m_View.getContext(), R.layout.list_item, R.id.text1, m_Items) {
-            @Override
-            public View getView(int position, View convertView, ViewGroup parent) {
-                View view = super.getView(position, convertView, parent);
-
-                ((TextView) view.findViewById(R.id.text1)).setText(m_Items.get(position).toString());
-                ((TextView) view.findViewById(R.id.text2)).setText(m_Items.get(position).getTitle());
-                view.findViewById(R.id.checked).setVisibility(m_Items.get(position).isEnabled() ? View.VISIBLE : View.GONE);
-
-                return view;
-            }
-        };
         txtTimer = (TextView)m_View.findViewById(R.id.txtTimeValue);
+
+        listView = (ListView) m_View.findViewById(R.id.listLap);
+        arrayList = new ArrayList<>();
+        adapter = new ArrayAdapter<String>(m_View.getContext(), R.layout.row, R.id.txtContent, arrayList);
+        listView.setAdapter(adapter);
 
         btnStart = (Button) m_View.findViewById(R.id.btnStart);
         btnStart.setOnClickListener(new View.OnClickListener() {
@@ -100,16 +87,19 @@ public class TabStopwatch extends TabFragment
                 }
                 else if(btnStart.getText() == "Stop"){
                     customHandler.removeCallbacks(updateTimerThread);
+                    txtTimer.setText(toText(txtTimer.getText().toString()));
                     btnStart.setText("Reset");
                 }
                 else {
                     btnStart.setText("Start");
                     btnPause.setText("Pause");
-                    txtTimer.setText("00:00:00:00");
+                    txtTimer.setText("00:00:00");
                     startTime = 0L;
                     timeInMilliseconds = 0L;
                     timeSwapBuff = 0L;
                     updateTime = 0L;
+                    arrayList.clear();
+                    adapter.notifyDataSetChanged();
                 }
             }
         });
@@ -118,10 +108,11 @@ public class TabStopwatch extends TabFragment
         btnPause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(btnStart.getText()!="Reset") {
+                if(btnStart.getText()!= "Reset" && btnStart.getText() != "Start") {
                     if (btnPause.getText() == "Pause") {
                         btnPause.setText("Unpause");
                         timeSwapBuff += timeInMilliseconds;
+                        txtTimer.setText(toText(txtTimer.getText().toString()));
                         customHandler.removeCallbacks(updateTimerThread);
                     } else {
                         btnPause.setText("Pause");
@@ -136,22 +127,58 @@ public class TabStopwatch extends TabFragment
         btnLap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                /*if(btnStart.getText()!="Reset") {
-                    LayoutInflater inflater = (LayoutInflater)getBaseContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                    View addView = inflater.inflate(R.layout.row, null);
-                    TextView txtValue = (TextView)addView.findViewById(R.id.txtContent);
-                    txtValue.setText(txtTimer.getText());
-                    container.addView(addView);
-                }*/
+                if(btnStart.getText()!= "Reset" && btnStart.getText() != "Start") {
+                    arrayList.add(0, toText(txtTimer.getText().toString()));
+                    adapter.notifyDataSetChanged();
+                }
             }
         });
 
     }
 
+    protected String toText(String txt){
+        String txtString[] = txt.split(":");
+        int checker = 0;
+        for(int i = 0; i < 3; i++){     //loop 3 times to get hours/mins/secs/millisecs
+            for(int j = 0; j < 20; j++){ //loop checks between 00-19
+                if(txtString[i].equals(intNumbers[j])){
+                    txtString[i] = txtSingleNumber[j];
+                    checker = 1;
+                    j=20;
+                }
+            }
+            if(checker != 1) {
+                String nr[] = new String[2];
+                int tmp;
+                tmp = Integer.parseInt(txtString[i]);
+                nr[1] = ""+tmp%10;
+                nr[0] = ""+(tmp - Integer.parseInt(nr[1]))/10;
+                nr[1] = "0"+nr[1];
+                nr[0] = "0"+nr[0];
+
+                for(int j = 2; j < 10; j++){
+                    if(nr[0].equals(intNumbers[j])){
+                        txtString[i]=txtTenNumbers[j];
+                        j=10;
+                    }
+                }
+                if(nr[1].equals("00")) {
+                    for (int j = 0; j < 10; j++) {
+                        if (nr[1].equals(intNumbers[j])) {
+                            txtString[i] += "-" + txtSingleNumber[j];
+                            j = 10;
+                        }
+                    }
+                }
+            }
+            checker = 0;
+        }
+        return txtString[0]+":"+txtString[1]+":"+txtString[2];
+    }
+
     @Override
     public void onTimeUpdated()
     {
-
     }
 
     @Override
